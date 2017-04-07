f0c7e12fc7cd41ec82b6453c2122e366465cb0d3
thimonbongaards <thimonbongaards1@gmail.com>
2017-04-07 19:54:35 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "thimonbongaards",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/thimonbongaards/source/master/lang/en.json",
+      scriptLink: "https://rawgit.com/thimonbongaards/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 5, // 1-200
       startupVolume: 0, // 0-100
