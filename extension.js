7d331b62848d4b53d030aeea76ac2d7e4ee6fdc8
thimonbongaards <thimonbongaards1@gmail.com>
2017-04-07 19:35:42 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/thimonbongaards/source/master/extension.js", extend);
 
 }).call(this);
